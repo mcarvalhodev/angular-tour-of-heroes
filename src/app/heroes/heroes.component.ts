c69fc01a70375603127f6baed918be1c88832c60
Marcelo Carvalho Santos <marcelosan11>
2019-04-26 13:31:58 -0300

    toh-tp2

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from './hero';
+import {HEROES} from './mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -13,9 +14,16 @@ export class HeroesComponent implements OnInit {
     name: 'Batman'
   }
 
+  heroes = HEROES;
+
+  selectedHero: Hero;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
